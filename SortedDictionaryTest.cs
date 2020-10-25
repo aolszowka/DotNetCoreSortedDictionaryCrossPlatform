8f4bd1b240b3126a69ae47c26cce0f8459e94076
Ace Olszowka <aolszowka@gmail.com>
2020-10-24 19:18:10 -0600

    Update SortedDictionaryTest.cs

--- SortedDictionaryTest.cs
+++ SortedDictionaryTest.cs
@@ -9,12 +9,12 @@ namespace DotNetCoreSortedDictionaryCrossPlatform
     public class SortedDictionaryTest
     {
         [Test]
-        public void Test1()
+        public void DifferenceBetweenPlatforms()
         {
             SortedDictionary<string, int> sortedDictionary = new SortedDictionary<string, int>();
 
-            sortedDictionary.Add("Microsoft.CodeAnalysis.VisualBasic.CodeStyle", 0);
-            sortedDictionary.Add("Microsoft.CodeAnalysis.VisualBasic", 0);
+            sortedDictionary.Add("Project(\"{778DAE3C-4631-46EA-AA77-85C1314464D9}\") = \"Microsoft.CodeAnalysis.VisualBasic.CodeStyle.Fixes\", \"src\\CodeStyle\\VisualBasic\\CodeFixes\\Microsoft.CodeAnalysis.VisualBasic.CodeStyle.Fixes.vbproj\", \"{0141285D-8F6C-42C7-BAF3-3C0CCD61C716}\"", 0);
+            sortedDictionary.Add("Project(\"{778DAE3C-4631-46EA-AA77-85C1314464D9}\") = \"Microsoft.CodeAnalysis.VisualBasic\", \"src\\Compilers\\VisualBasic\\Portable\\Microsoft.CodeAnalysis.VisualBasic.vbproj\", \"{2523D0E6-DF32-4A3E-8AE0-A19BFFAE2EF6}\"", 0);
 
             StringBuilder sb = new StringBuilder();
 
@@ -26,7 +26,9 @@ namespace DotNetCoreSortedDictionaryCrossPlatform
             string actual = sb.ToString();
 
             // This is how it will appear on Windows, but in Ubuntu this fails? (maybe?)
-            string expected = "Microsoft.CodeAnalysis.VisualBasic" + Environment.NewLine + "Microsoft.CodeAnalysis.VisualBasic.CodeStyle" + Environment.NewLine;
+            string expected =
+                "Project(\"{778DAE3C-4631-46EA-AA77-85C1314464D9}\") = \"Microsoft.CodeAnalysis.VisualBasic\", \"src\\Compilers\\VisualBasic\\Portable\\Microsoft.CodeAnalysis.VisualBasic.vbproj\", \"{2523D0E6-DF32-4A3E-8AE0-A19BFFAE2EF6}\"" + Environment.NewLine +
+                "Project(\"{778DAE3C-4631-46EA-AA77-85C1314464D9}\") = \"Microsoft.CodeAnalysis.VisualBasic.CodeStyle.Fixes\", \"src\\CodeStyle\\VisualBasic\\CodeFixes\\Microsoft.CodeAnalysis.VisualBasic.CodeStyle.Fixes.vbproj\", \"{0141285D-8F6C-42C7-BAF3-3C0CCD61C716}\"" + Environment.NewLine;
 
             Assert.That(actual, Is.EqualTo(expected));
         }
